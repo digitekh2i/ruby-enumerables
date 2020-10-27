ed577530b21520ccbe9a444b49dfaf2a9bfcaeb9
Janluc <Janlucsaneaux@gmail.com>
2020-10-27 12:20:29 -0400

    Declare and define my_each_with_index function

--- main.rb
+++ main.rb
@@ -1,15 +1,30 @@
 
 module Enumerable 
-    def my_each
-      counter = 0
-      while counter < self.size
-        if self == Hash
-          new_hash = self.to_a
-          yield(new_hash[counter])
-        else 
-         yield(self[counter])
-        end
-        counter += 1
+  def my_each
+    counter = 0
+    while counter < self.size
+      if self == Hash
+        new_hash = self.to_a
+        yield(new_hash[counter])
+      else 
+        yield(self[counter])
       end
+      counter += 1
     end
   end
+
+  def my_each_with_index
+    counter = 0
+    while counter < self.size
+      if self == Hash || self == Range
+        new_hash = self.to_a
+        yield(new_hash[counter], counter)
+      else 
+        yield(self[counter], counter)
+      end
+      counter += 1
+    end
+  end
+  
+end
+  
